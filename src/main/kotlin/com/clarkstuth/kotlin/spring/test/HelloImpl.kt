4073eef29c9a25a1287ef12a8128564cc81f8c9d
Clark Stuth <clarkstuth@gmail.com>
2016-06-22 06:19:00 -0500

    actually using config specified bean

--- src/main/kotlin/com/clarkstuth/kotlin/spring/test/HelloImpl.kt
+++ src/main/kotlin/com/clarkstuth/kotlin/spring/test/HelloImpl.kt
@@ -1,12 +1,14 @@
 package com.clarkstuth.kotlin.spring.test
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
-class HelloImpl {
+class HelloImpl @Autowired constructor(@Qualifier("output.string") val outputString: String) {
 
     fun sayHello() {
-        println("Hello!")
+        println(outputString)
     }
 
 }
